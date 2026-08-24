82f5e263fa7947b444f90a99bae21c6178c9babf
Sophia Blair <sophb.ccjt@proton.me>
2026-08-24 07:44:28 -0300

    Update version to 1.7.0 and add randomMinMax method

--- templates/protoplus.filler.js
+++ templates/protoplus.filler.js
@@ -64,6 +64,7 @@ const protoplus = {
 				degToRad: (deg) => (deg * Math.PI) / 180,
 			},
 			root: (radicand, index = 2) => radicand ** 1 / index,
+			randomMinMax: (min, max) => Math.random() * (max - min) + min;
 		},
 
 		Number: {
@@ -741,5 +742,5 @@ const protoplus = {
 		const endTime = now();
 		console.log(`contracted methods in ${endTime - startTime}ms`);
 	},
-	version: "1.6.0",
+	version: "1.7.0",
 };
